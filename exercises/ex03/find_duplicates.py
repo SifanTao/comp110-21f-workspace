c3bbff42545875a7507a2884b543a4bb29c7bc33
Sifan Tao <stao1@unc.edu>
2021-09-14 12:56:07 -0400

    EX03 progress

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,25 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730529631"
+
+
+word: str = input("Enter a word: ")
+i: int = 0
+x: int = 0
+
+while i < len(word):
+    x = i + 1
+    while x < len(word):
+        if word[i] == word[x]:
+            print("Found duplicate: True")
+            quit()
+        else:
+            x = x + 1
+    i = i + 1
+    
+
+print("Found duplicate: False")
+
+
+
+
